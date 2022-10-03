7473f307ea5af99190f1aa505e657b0231e40d7e
slothsoft <admin@slothsoft.de>
2022-10-03 20:14:16 +0200

    [12] add release script

--- README.md
+++ README.md
@@ -61,6 +61,16 @@ To start developing this mod, you need to
 
 1. Create [stardewvalley.targets](https://github.com/Pathoschild/SMAPI/blob/develop/docs/technical/mod-package.md#custom-game-path) file with the game folder
 
+### Release
+
+1. Check that all the versions are correct (see point 5)
+2. Run _build.bat_, which only really works on my PC, but so what: 
+```bat
+.\build x.x.x
+```
+3. Put the contents of _bin/Challenger.zip_ in a fresh Stardew Valley and test if everything works
+4. Create a new tag and release on GitHub, append the ZIP
+5. Increment the version in _manifest.json_ and _Challenger.csproj_
 
 ### Used Tutorials
 
---
+++ build.bat
@@ -0,0 +1,19 @@
+﻿
+Rem Release the entire project
+Rem dotnet publish -c Release
+
+Rem Create a folder that contains everything that should be in the ZIP
+set projectFolder=%cd%\Challenger
+set zipFolder=%projectFolder%\bin\ReleaseZip
+set outputFolder=%zipFolder%\Challenger
+if exist "%zipFolder%" rmdir /s /q "%zipFolder%"
+if not exist "%outputFolder%" mkdir "%outputFolder%"
+
+xcopy /y %projectFolder%\bin\Release\net5.0\publish\Challenger.dll %outputFolder%
+xcopy /y %projectFolder%\bin\Release\net5.0\publish\Challenger.pdb %outputFolder%
+xcopy /y %projectFolder%\manifest.json %outputFolder%
+xcopy /y %projectFolder%\i18n\ %outputFolder%\i18n\
+xcopy /y %cd%\LICENSE %outputFolder%
+xcopy /y %cd%\README.md %outputFolder%
+
+"C:\Program Files\7-Zip\7z.exe" a %zipFolder%\Challenger-%1.zip %zipFolder%/*
